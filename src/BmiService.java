f53dfdf989cf7904919a2733eef3813a983a1e3c
Aleksandr <tsetserev123@gmail.com>
2022-05-29 17:37:58 +0300

    улучшеная версия

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
     public double bmi(double h, double m){
-        double growth = h * h;
-        double weight = m / growth;
-        double index = weight * 10000;
+        double index = m / (h * h) * 10000;
         return index;
     }
 
